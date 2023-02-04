dc0af5eae414374fe274f0691ebeb387c731d384
MaySuKyiNaing <maysukyinaing@gmail.com>
2023-02-04 22:20:42 +0630

    container between directive is embedded container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+enum Visibility{
+  On = 'on',
+  Off = 'off'
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'if-not-directive';
+
+  visibility ?: Visibility
+  VISIBILITY = Visibility
+
+  ngOnInit(): void {
+    this.setVisibility(this.VISIBILITY.Off)
+  }
+
+  setVisibility(value:Visibility) {
+    this.visibility = value
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IfNotDirective } from './directives/if-not.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IfNotDirective
   ],
   imports: [
     BrowserModule
